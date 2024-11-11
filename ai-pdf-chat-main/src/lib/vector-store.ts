2b6f519fa7a5b1af039ea7ed2eb442971dbe253f
Maksym Tutskyi <armagedonec1996@gmail.com>
2024-11-11 21:00:53 +0200

    AI pdf chat

---
+++ ai-pdf-chat-main/src/lib/vector-store.ts
@@ -0,0 +1,35 @@
+import {
+  Document,
+  VectorStoreIndex,
+  serviceContextFromDefaults,
+  storageContextFromDefaults,
+} from "llamaindex";
+
+export async function createOrReadVectorStoreIndex(
+  pdfText?: string,
+): Promise<VectorStoreIndex> {
+  try {
+    const document = new Document({ text: pdfText});
+
+  const storageContext = await storageContextFromDefaults({
+    persistDir: "./storage",
+  });
+
+  const serviceContext = serviceContextFromDefaults({
+    chunkSize: 2000,
+    chunkOverlap: 500,
+  });
+
+  console.log("Creating index");
+
+  const index = await VectorStoreIndex.fromDocuments(pdfText ? [document] : [], {
+    storageContext,
+    serviceContext,
+  });
+
+  console.log("Index created");
+  return index;
+  } catch (error) {
+    console.log('error vector', error);
+  }
+}
